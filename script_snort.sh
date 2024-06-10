555c3aa2fd9430b073e32a654c8ee26887eed140
Vincent Lefebvre <Rikall99>
2024-06-10 06:06:17 +0200

    AAAAAAAAAAAAAAAAAAAAAAAH

--- script_snort.sh
+++ script_snort.sh
@@ -11,7 +11,9 @@ sudo chmod 600 /etc/snort/snort.debian.conf
 sudo DEBIAN_FRONTEND=noninteractive apt install snort -yq
 
 echo 'alert ip any any -> any any (msg:"SQL Injection Attempt"; content:"select"; nocase; classtype:attempted-recon; sid:1000001;)' | sudo tee -a /etc/snort/rules/local.rules
-echo 'alert ip any any -> any any (msg:”Possible SQL Injection — Inline Comments Detected”; flow:to_server,established; content:”GET”; nocase; http_method; content:”/”; http_uri; pcre:”/?.( — |#|/*)/”; sid:1000002;)' | sudo tee -a /etc/snort/rules/local.rules
-echo 'alert ip any any -> any any (msg:”Possible SQL Injection — UNION keyword detected”; flow:to_server,established; content:”UNION”; nocase; http_uri; sid:1000003;)' | sudo tee -a /etc/snort/rules/local.rules
+echo 'alert ip any any -> any any (msg:"Possible SQL Injection — UNION keyword detected"; flow:to_server,established; content:"UNION"; nocase; http_uri; sid:1000002;)' | sudo tee -a /etc/snort/rules/local.rules
+echo 'alert ip any any -> any any (msg:"SQL Injection Attempt"; content:"1 = 1"; nocase; classtype:attempted-recon; sid:1000003;)' | sudo tee -a /etc/snort/rules/local.rules
+echo 'alert ip any any -> any any (msg:"SQL Injection Attempt"; content:"or"; nocase; classtype:attempted-recon; sid:1000004;)' | sudo tee -a /etc/snort/rules/local.rules
+
 
 sudo service snort start
